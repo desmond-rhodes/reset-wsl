7c008012a0abaf6776f30f38c81d4b6473708b2d
Desmond Rhodes <desmond.rhodes@outlook.com>
2025-05-31 01:14:51 +0700

    Install distro and setup user

---
+++ install-distro.bat
@@ -0,0 +1,18 @@
+@echo off
+
+if "%~1"=="" (set "distro=debian") else (set "distro=%~1")
+if "%~2"=="" (set "location=C:\WSL") else (set "location=%~2")
+if "%~3"=="" (set "image=debian.tar") else (set "image=%~3")
+
+echo distro: %distro%
+echo location: %location%
+echo image: %image%
+echo:
+
+wsl --unregister %distro% > nul 2>&1
+
+mkdir "%location%" > nul 2>&1
+wsl --import %distro% "%location%\%distro%" "%image%" > nul
+
+wsl -d %distro% -u root "./setup-user"
+wsl -t %distro% > nul 2>&1
